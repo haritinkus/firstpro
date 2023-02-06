2a6c38a24d5fd2328dc9a21bd360117d0321a7a5
Harish Kailasa <hari.kailasa@gmail.com>
2023-02-07 05:07:07 +0530

    sixth commit

--- src/main/java/firstpro/SecondClass.java
+++ src/main/java/firstpro/SecondClass.java
@@ -5,7 +5,7 @@ public class SecondClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-
+		System.out.println("Hello All");
 	}
 
 }
